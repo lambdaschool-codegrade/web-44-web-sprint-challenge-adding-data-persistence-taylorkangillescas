5d3908be018db40bc37fd2f5b98f9d9c3f015a13
Taylor Kang-Illescas <66800091+taylorkangillescas@users.noreply.github.com>
2021-08-13 15:14:19 -0700

    should be done, having issues with cg tests

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,46 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getProjects() {
+    const results = await db('projects as p')
+        .select('p.project_id', 'p.project_name', 'p.project_description', 'p.project_completed')
+        
+    const projects = []
+
+    results.forEach(result => {
+        projects.push({
+            project_id: result.project_id,
+            project_name: result.project_name,
+            project_description: result.project_description,
+            project_completed: Boolean(result.project_completed)
+        })
+    });
+    
+    return projects
+}
+
+async function addProject(project) {
+    const results = await db('projects as p').insert(project)
+
+    let newProject = await db('projects as p')
+    .select(
+        'p.project_id', 
+        'p.project_name', 
+        'p.project_description', 
+        'p.project_completed'
+        )
+    .where('p.project_id', results)
+    .first()
+
+    newProject = {
+        ...newProject,
+        project_completed: Boolean(newProject.project_completed)
+    }
+    
+    return newProject
+}
+
+module.exports = {
+    getProjects,
+    addProject,
+};
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,60 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+async function getTasks() {
+    const results = await db('tasks as t')
+        .leftJoin('projects as p', 'p.project_id', 't.project_id')
+        .select(
+            't.task_id', 
+            't.task_description', 
+            't.task_notes', 
+            't.task_completed', 
+            'p.project_name', 
+            'p.project_description'
+            )
+
+    const tasks = []
+
+    results.forEach(result => {
+        tasks.push({
+            task_id: result.task_id,
+            task_description: result.task_description,
+            task_notes: result.task_notes,
+            task_completed: Boolean(result.task_completed),
+            project_name: result.project_name,
+            project_description: result.project_description
+        })
+    })
+
+    return tasks
+}
+
+async function addTask(task) {
+    const results = await db('tasks as t').insert(task)
+    
+    let newTask = await db('tasks as t')
+        .leftJoin('projects as p', 'p.project_id', 't.project_id')
+        .select(
+                't.task_id', 
+                't.task_description', 
+                't.task_notes', 
+                't.task_completed', 
+                'p.project_name', 
+                'p.project_description'
+                )
+        .where('t.task_id', results)
+        .first()
+    
+    newTask = {
+        ...newTask,
+        task_completed: Boolean(newTask.task_completed)
+    }
+    
+    return newTask
+}
+
+module.exports = {
+    getTasks,
+    addTask,
+};
+
